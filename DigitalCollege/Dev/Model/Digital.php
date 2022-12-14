f320bb39116b714a6ef3bf7c74c1954a6e37436d
SaorCampos <113113310+SaorCampos@users.noreply.github.com>
2022-12-14 11:01:26 -0300

    Aula do dia 14

---
+++ DigitalCollege/Dev/Model/Digital.php
@@ -0,0 +1,15 @@
+<?php
+namespace DigitalCollege\Dev\Model;
+use Magento\Framework\Model\AbstractModel;
+
+class Digital extends AbstractModel
+{
+    /**
+     * Initialize resource model
+     * @return void
+     */
+    public function _construct()
+    {
+        $this->_init('DigitalCollege\Dev\Model\ResourceModel\Digital');
+    }
+}
